95d79dfe386f9402a6cc3e9ed2d842020d52669b
Nader <naderamama2022@gmail.com>
2024-03-26 02:43:04 +0100

    commit 3

--- src/app/components/quatriemme/9essa/kessa1/kessa1.component.ts
+++ src/app/components/quatriemme/9essa/kessa1/kessa1.component.ts
@@ -4,16 +4,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-kessa1',
   templateUrl: './kessa1.component.html',
-  styleUrls: ['./kessa1.component.css']
+  styleUrls: ['./kessa1.component.css'],
 })
 export class Kessa1Component {
   audioElement: HTMLAudioElement;
   isPlaying: boolean;
 
-  constructor(private router : Router) {
-    this.audioElement = new Audio("assets/vocal/أمي حدثيني عن فلسطين.mp3");
+  constructor(private router: Router) {
+    this.audioElement = new Audio('assets/vocal/palestine.mp3');
     this.isPlaying = false;
-
   }
 
   toggleAudio() {
@@ -24,9 +23,7 @@ export class Kessa1Component {
     }
     this.isPlaying = !this.isPlaying;
   }
-routerFelastin() {
-  this.router.navigate(["/quest42"]);
-}
-
-
+  routerFelastin() {
+    this.router.navigate(['/quest42']);
+  }
 }
